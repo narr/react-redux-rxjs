dfc377387abd7d8f6b310c4aaa323aa711157f44
narr <nardgu@gmail.com>
2017-11-23 22:23:06 +0800

    after npm run eject

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import { jsdom } from 'jsdom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const document = jsdom();
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
