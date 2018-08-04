ecdf55bf2049b4caefcdc8a31d0b050937985adc
narr <nardgu@gmail.com>
2018-08-05 02:18:39 +0800

    build(webpack): add webpack for development and build

---
+++ static/app.bab71a33.js
@@ -0,0 +1,106 @@
+webpackJsonp(
+  [0],
+  {
+    181: function(e, t, o) {
+      Object.defineProperty(t, '__esModule', { value: !0 });
+      var r = o(1),
+        n = o(18),
+        a = o(182);
+      n.render(
+        r.createElement(a.default, null),
+        document.getElementById('root')
+      );
+    },
+    182: function(e, t, o) {
+      function r() {}
+      Object.defineProperty(t, '__esModule', { value: !0 });
+      var n,
+        a = o(1),
+        l = (o(62), o(183)),
+        s = function() {
+          return a.createElement(l.default, { onClick: r }, 'Test Button Base');
+        };
+      (n = s), (t.default = n);
+    },
+    183: function(e, t, o) {
+      Object.defineProperty(t, '__esModule', { value: !0 });
+      var r = o(184);
+      t.default = r.default;
+    },
+    184: function(e, t, o) {
+      function r() {}
+      var n =
+          (this && this.__assign) ||
+          Object.assign ||
+          function(e) {
+            for (var t, o = 1, r = arguments.length; o < r; o++) {
+              t = arguments[o];
+              for (var n in t)
+                Object.prototype.hasOwnProperty.call(t, n) && (e[n] = t[n]);
+            }
+            return e;
+          },
+        a =
+          (this && this.__rest) ||
+          function(e, t) {
+            var o = {};
+            for (var r in e)
+              Object.prototype.hasOwnProperty.call(e, r) &&
+                t.indexOf(r) < 0 &&
+                (o[r] = e[r]);
+            if (null != e && 'function' == typeof Object.getOwnPropertySymbols)
+              for (
+                var n = 0, r = Object.getOwnPropertySymbols(e);
+                n < r.length;
+                n++
+              )
+                t.indexOf(r[n]) < 0 && (o[r[n]] = e[r[n]]);
+            return o;
+          };
+      Object.defineProperty(t, '__esModule', { value: !0 });
+      var l = o(61),
+        s = o(2),
+        i = o(1),
+        c = function(e) {
+          var t = 'white';
+          return (
+            e.bootstrap && e.bootstrap.color && (t = e.bootstrap.color[800]),
+            {
+              root: {
+                background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+                border: 0,
+                borderRadius: 3,
+                boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+                color: t,
+                cursor: 'pointer',
+                height: 48,
+                padding: '0 30px',
+                '&:hover': { color: 'red' },
+                '&&:hover': { color: 'black' },
+              },
+            }
+          );
+        },
+        u = function(e) {
+          var t = e.classes,
+            o = a(e, ['classes']);
+          return i.createElement(
+            'button',
+            n({ type: 'button', className: t.root }, o)
+          );
+        },
+        f = l.withStyles(c)(u);
+      (f.propTypes = {
+        classes: s.objectOf(s.string),
+        onClick: s.func.isRequired,
+        title: s.string,
+      }),
+        (f.defaultProps = { title: 'this is a default browser tooltip' }),
+        (f.displayName = 'TestButtonBase'),
+        (t.default = f),
+        console.log(r);
+    },
+  },
+  [181]
+);
+//# sourceMappingURL=app.bab71a33.js.map
